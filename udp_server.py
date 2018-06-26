03b2d05581f74494a09cf049be2cd0e01c955c47
Judith <boldt.ju@gmail.com>
2018-06-26 14:25:10 +0200

    UDP server and client for testing

---
+++ udp_server.py
@@ -0,0 +1,24 @@
+import socket
+from struct import *
+from collections import namedtuple
+
+server_ip = '192.168.1.32'
+server_port = 1234
+
+serverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+serverSock.bind((server_ip, server_port))
+
+Sensors = namedtuple('Sensors', 'message_type light temperature humidity client_name_length client_name')
+
+while True:
+    raw_data, addr = serverSock.recvfrom(500)
+    
+    fmt_1 = '4sfffc'
+    data = Sensors._make(unpack(fmt_1, raw_data[:17]) + (b'',))
+    
+    l = int(data.client_name_length)
+    fmt_2 = '<' + str(l) + 's'
+    client_name = unpack(fmt_2, raw_data[17:17+l])[0]
+    data = data._replace(client_name=client_name)
+    print(data)
+    
